e2680b7a8bec13829d55d5b2b7600723109d78d5
hchmr <73483158+hchmr@users.noreply.github.com>
2024-05-10 00:16:30 +0200

    Add basic 'go to definition' to extension

--- vscode-cog/extension.js
+++ vscode-cog/extension.js
@@ -1,4 +1,4 @@
-const { window, languages, workspace, Diagnostic, DiagnosticSeverity, Range } = require('vscode');
+const { window, languages, workspace, Diagnostic, DiagnosticSeverity, Range, Location, SymbolInformation, SymbolKind } = require('vscode');
 const { spawn } = require('child_process');
 
 // @ts-check
@@ -7,6 +7,7 @@ const { spawn } = require('child_process');
 /** @typedef {import('vscode').OutputChannel} OutputChannel */
 /** @typedef {import('vscode').TextDocument} TextDocument */
 /** @typedef {import('vscode').DiagnosticCollection} DiagnosticCollection */
+/** @typedef {import('vscode').Definition} Definition */
 
 module.exports = {
     activate,
@@ -23,7 +24,7 @@ let diagnosticsCollection;
  */
 function activate(context) {
     context.subscriptions.push(log = window.createOutputChannel('Cog'));
-    log.show();
+    setTimeout(() => log.show(), 1000);
 
     log.appendLine('Cog activated');
 
@@ -33,6 +34,27 @@ function activate(context) {
     workspace.onDidOpenTextDocument(e => refreshDiagnostics(e), null, context.subscriptions);
     workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document), null, context.subscriptions);
     workspace.textDocuments.forEach(refreshDiagnostics);
+
+    context.subscriptions.push(languages.registerDefinitionProvider('cog', {
+        provideDefinition(document, position) {
+            return searchDefinitions(document, position);
+        },
+    }));
+
+    context.subscriptions.push(languages.registerDocumentSymbolProvider('cog', {
+        provideDocumentSymbols(document) {
+            return findDefinitionsInSource(document).map(def =>
+                new SymbolInformation(
+                    def.name,
+                    def.kind,
+                    undefined,
+                    new Location(document.uri, new Range(
+                        document.positionAt(def.index),
+                        document.positionAt(def.index + def.name.length)
+                    ))
+                ));
+        },
+    }));
 }
 
 /**
@@ -100,4 +122,55 @@ async function compile(document) {
     log.appendLine(`Compiler exited with code ${code}`);
 
     return { ok: code == 0, stderr };
+}
+
+function searchDefinitions(document, position) {
+    const definitions = findDefinitionsInSource(document);
+    const word = document.getText(document.getWordRangeAtPosition(position));
+    log.appendLine(`Looking for definition of '${word}'`);
+    const filtered = definitions.filter(def => def.name === word).map(def => new Location(
+        document.uri,
+        new Range(
+            document.positionAt(def.index),
+            document.positionAt(def.index + def.name.length)
+        )
+    ));
+    log.appendLine(`Found definitions: ${JSON.stringify(filtered)}`);
+    return filtered;
+}
+
+/**
+ * @param {TextDocument} document
+ */
+function findDefinitionsInSource(document) {
+    const definitions = [];
+
+    for (const match of document.getText().matchAll(/^(struct|func|var|const)\s*(\w+)/gm)) {
+        definitions.push({
+            index: match.index + match[0].length - match[2].length,
+            name: match[2],
+            kind: {
+                'struct': SymbolKind.Struct,
+                'func': SymbolKind.Function,
+                'var': SymbolKind.Variable,
+                'const': SymbolKind.Constant,
+            }[match[1]],
+        });
+    }
+
+    // enums
+    for (const match of document.getText().matchAll(/^enum\s*\{([^}]*)\}/gm)) {
+        for (const part of match[1].split(',')) {
+            const name = part.trim();
+            if (name) {
+                definitions.push({
+                    index: match.index + match[0].indexOf(name),
+                    name: name,
+                    kind: SymbolKind.EnumMember,
+                });
+            }
+        }
+    }
+
+    return definitions;
 }
