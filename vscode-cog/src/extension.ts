b6f889886fc2a7e9697cc3c1d15ddc351cbc8607
hchmr <73483158+hchmr@users.noreply.github.com>
2024-07-17 20:12:53 +0200

    Begin integrating tree-sitter into extension

--- vscode-cog/src/extension.ts
+++ vscode-cog/src/extension.ts
@@ -1,5 +1,25 @@
 import * as vscode from 'vscode';
+import Parser from 'tree-sitter';
+import Cog from 'tree-sitter-cog';
+
+const parser = new Parser();
+parser.setLanguage(Cog);
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage('Hello World from vscode-cog!');
+
+    context.subscriptions.push(
+        vscode.languages.registerHoverProvider('cog', {
+            provideHover(document, position, token) {
+                const tree = parser.parse(document.getText());
+                const treePosition = { row: position.line, column: position.character };
+                const node = tree.rootNode.namedDescendantForPosition(treePosition);
+                if (node) {
+                    return new vscode.Hover(node.type);
+                } else {
+                    return new vscode.Hover('No node found');
+                }
+            }
+        })
+    );
 }
