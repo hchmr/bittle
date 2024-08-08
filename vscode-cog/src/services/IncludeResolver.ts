12a4a1351b64af623761df6f932944632bddf568
hchmr <73483158+hchmr@users.noreply.github.com>
2024-08-08 22:57:48 +0200

    Fix IncludeResolver crash on invalid string literal

--- vscode-cog/src/services/IncludeResolver.ts
+++ vscode-cog/src/services/IncludeResolver.ts
@@ -5,14 +5,29 @@ import { VirtualFileSystem } from '../vfs';
 export class IncludeResolver {
     constructor(private vfs: VirtualFileSystem) { }
 
-    resolveInclude(filePath: string, arg: string | SyntaxNode) {
+    resolveInclude(filePath: string, arg: string | SyntaxNode): string | undefined {
         const pathValue
             = typeof arg === 'string'
                 ? arg
-                : JSON.parse(arg.text);
-        const includePath = path.resolve(path.dirname(filePath), pathValue);
-        if (this.vfs.exists(includePath)) {
-            return includePath;
+                : tryParseString(arg.text);
+        if (!pathValue) {
+            return;
         }
+        const includePath = path.resolve(path.dirname(filePath), pathValue);
+        if (!this.vfs.exists(includePath)) {
+            return;
+        }
+        return includePath;
+    }
+}
+
+function tryParseString(text: string): string | undefined {
+    if (!text.startsWith('"')) {
+        return;
+    }
+    try {
+        return JSON.parse(text);
+    } catch {
+        return;
     }
 }
