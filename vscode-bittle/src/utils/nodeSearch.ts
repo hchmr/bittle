a957a4989d49da53c3dec6ad4a0f99ecdbd93a00
hchmr <73483158+hchmr@users.noreply.github.com>
2024-10-13 18:31:30 +0200

    refact(ide): Remove unused function

--- vscode-bittle/src/utils/nodeSearch.ts
+++ vscode-bittle/src/utils/nodeSearch.ts
@@ -14,22 +14,6 @@ export function nodeEndsAt(position: Point, node: SyntaxNode): boolean {
     return pointEq(node.endPosition, position);
 }
 
-export function nodeContains(x1: SyntaxNode, x2: SyntaxNode): boolean {
-    if (rangeContains(x1, x2)) {
-        return true;
-    }
-
-    let node: SyntaxNode | null = x2;
-    do {
-        if (node === x1) {
-            return true;
-        }
-        node = node.parent;
-    } while (node);
-
-    return false;
-}
-
 export function countPrecedingCommas(argsNodes: SyntaxNode[], treePosition: Point) {
     return argsNodes
         .filter((argNode) =>
