415b667809c22c1f0b909a67ce8d1f1c42cb7555
hchmr <73483158+hchmr@users.noreply.github.com>
2025-02-16 20:56:35 +0100

    feat(ide): Show doc comments on hover

--- vscode-bittle/src/features/hover.ts
+++ vscode-bittle/src/features/hover.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
-import { prettySym, Sym, SymKind, symRelatedType } from '../semantics/sym';
+import { Origin, prettySym, Sym, SymKind, symRelatedType } from '../semantics/sym';
 import { prettyType, Type, typeLayout, TypeLayout } from '../semantics/type';
 import { ParsingService } from '../services/parsingService';
 import { SemanticsService } from '../services/semanticsService';
 import { SyntaxNode } from '../syntax';
 import { isExprNode, isTypeNode } from '../syntax/nodeTypes';
+import { TokenNodeImpl } from '../syntax/treeImpl';
+import { dedent } from '../utils';
 import { interceptExceptions } from '../utils/interceptExceptions';
 import { toVscRange } from '../utils/vscode';
 
@@ -80,14 +82,55 @@ type HoverDetail =
     | { kind: 'type'; type: Type; node: SyntaxNode; layout?: TypeLayout };
 
 function toHover(hoverDetail: HoverDetail): vscode.Hover {
-    let text = hoverDetail.kind === 'sym'
-        ? hoverDetail.syms.map(prettySym).join('\n')
-        : prettyType(hoverDetail.type);
+    const markdownString = new vscode.MarkdownString();
 
-    if (hoverDetail.layout) {
-        const { size, align } = hoverDetail.layout;
-        text += ` // size = ${size}, align = ${align}`;
+    if (hoverDetail.kind === 'sym') {
+        for (const sym of hoverDetail.syms) {
+            const docs = sym.origins.map(getDoc).filter(Boolean);
+            for (const doc of docs) {
+                markdownString.appendMarkdown(doc + '\n\n');
+            }
+            markdownString.appendCodeblock(prettySym(sym) + layoutDetail(hoverDetail), 'bittle');
+        }
+    } else {
+        markdownString.appendCodeblock(prettyType(hoverDetail.type) + layoutDetail(hoverDetail), 'bittle');
     }
 
-    return new vscode.Hover(new vscode.MarkdownString().appendCodeblock(text, 'bittle'), toVscRange(hoverDetail.node));
+    return new vscode.Hover(markdownString, toVscRange(hoverDetail.node));
+
+    function layoutDetail(hoverDetail: HoverDetail): string {
+        if (!hoverDetail.layout) {
+            return '';
+        }
+        const { size, align } = hoverDetail.layout;
+        return ` /* size: ${size}, align: ${align} */`;
+    }
+}
+
+function getDoc(origin: Origin): string {
+    const node = origin.node;
+
+    // TODO: should there be a tree method to get the list of tokens for a node?
+    const smallestNode = node.descendantForPosition(node.startPosition);
+    if (!(smallestNode instanceof TokenNodeImpl)) {
+        return '';
+    }
+
+    const startColumn = smallestNode.startPosition.column;
+    const lineRegexp = new RegExp(`^\\s{${startColumn}}//(.*)`);
+
+    const triviaLines = smallestNode.token.leadingTrivia.join('').split('\n');
+    triviaLines.pop(); // Same line as the node itself
+
+    const docLines: string[] = [];
+    for (const line of triviaLines.reverse()) {
+        const match = line.match(lineRegexp);
+        if (!match) {
+            break;
+        }
+        docLines.push(match[1]);
+    }
+    docLines.reverse();
+
+    return dedent(docLines.join('\n'));
 }
