32e15464b932317416578db52ee69e8062179295
hchmr <73483158+hchmr@users.noreply.github.com>
2025-03-21 02:16:42 +0100

    feat(ide): Add autocomplete for module imports

---
+++ vscode-bittle/src/services/moduleListService.ts
@@ -0,0 +1,29 @@
+import { NodeTypes } from '../syntax/nodeTypes';
+import { ReactiveCache } from '../utils/reactiveCache';
+import { ParsingService } from './parsingService';
+import { VirtualFileSystem } from './vfs';
+
+export class ModuleListService {
+    constructor(
+        private parsingService: ParsingService,
+        private vfs: VirtualFileSystem,
+        private cache: ReactiveCache,
+    ) { }
+
+    getModuleList(): string[] {
+        return this.cache.compute(`moduleList`, () => {
+            return Array.from(this.getModuleListUncached());
+        });
+    }
+
+    private *getModuleListUncached(): Iterable<string> {
+        for (const filePath of this.vfs.listFiles()) {
+            const tree = this.parsingService.parse(filePath);
+            for (const node of tree.rootNode.children) {
+                if (node.type === NodeTypes.ModuleNameDecl) {
+                    yield filePath;
+                }
+            }
+        }
+    }
+}
